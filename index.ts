b58c505ef2495ab5f5f09adbb630e8e587f71d9f
kahosan <15116355657l@gmail.com>
2022-08-16 12:15:48 +0800

    feat: reply new link to old message

--- index.ts
+++ index.ts
@@ -8,7 +8,12 @@ const REGEX = /((?<=\/)twitter)/; // 匹配 twitter 链接
 const url = {
   getUpdates: `https://api.telegram.org/bot${TOKEN}/getUpdates?`,
   editMessageText: `https://api.telegram.org/bot${TOKEN}/editMessageText?`,
+  sendMessage: `https://api.telegram.org/bot${TOKEN}/sendMessage?`,
 };
+
+function replaceMessage(text: string) {
+  return text.replace(/twitter/, 'vxtwitter');
+}
 
 async function getChannelMessage(offset?: number): Promise<MessageUpdatesResp> {
   const parmas = new URLSearchParams({
@@ -39,7 +44,7 @@ async function getChannelMessage(offset?: number): Promise<MessageUpdatesResp> {
  */
 
 function editChannelMessage(messageId: number, chatId: number, text: string, offset: number) {
-  const replaceText = text.replace(/twitter/, 'vxtwitter');
+  const replaceText = replaceMessage(text);
   const params = new URLSearchParams({
     chat_id: chatId.toString(),
     message_id: messageId.toString(),
@@ -60,20 +65,45 @@ function editChannelMessage(messageId: number, chatId: number, text: string, off
   }, 1000 * offset);
 }
 
+// 再多些请求就把请求逻辑单独抽出来
+function replyMessage(originalMessageId: number, chatId: number, text: string, offset: number) {
+  const replaceText = replaceMessage(text);
+  const params = new URLSearchParams({
+    chat_id: chatId.toString(),
+    reply_to_message_id: originalMessageId.toString(),
+    text: replaceText,
+  });
+
+  setTimeout(async () => {
+    try {
+      const res = await fetch(url.sendMessage + params);
+      if (!res.ok) {
+        throw new Error(`${res.status} ${res.statusText}`);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    console.log(`reply message to ${originalMessageId}`);
+  }, 1000 * offset);
+}
+
 let lastUpdateId: number; // 最后一条消息的 id，请求时 + 1 表示获取的更新已处理
 setInterval(async () => {
   try {
     const data = (await getChannelMessage(lastUpdateId)).result;
     lastUpdateId = data[data.length - 1]?.update_id + 1;
 
-    const filterData: { message_id: number; chat_id: number; text: string }[] = data
-      .filter(({ message, channel_post }) =>
-        /** 不能编辑群组中其它人的消息，以后要是预览全坏掉了就 reply 新链接吧
-        REGEX.test(message?.text) || **/ REGEX.test(channel_post?.text)
-      )
+    const filterData: {
+      type: 'message' | 'channel_post';
+      message_id: number;
+      chat_id: number;
+      text: string;
+    }[] = data
+      .filter(({ message, channel_post }) => REGEX.test(message?.text) || REGEX.test(channel_post?.text))
       .map(({ message, channel_post }) => {
-        // 懒得改了
         return {
+          type: message ? 'message' : 'channel_post',
           message_id: message?.message_id ?? channel_post?.message_id,
           chat_id: message?.chat.id ?? channel_post?.chat.id,
           text: message?.text ?? channel_post?.text,
@@ -85,7 +115,11 @@ setInterval(async () => {
 
     if (messageNumber) {
       filterData.forEach((item, index) => {
-        editChannelMessage(item.message_id, item.chat_id, item.text, index);
+        if (item.type === 'channel_post') {
+          editChannelMessage(item.message_id, item.chat_id, item.text, index);
+        } else {
+          replyMessage(item.message_id, item.chat_id, item.text, index);
+        }
       });
     }
   } catch (e) {
